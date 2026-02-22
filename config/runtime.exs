372300feb8635e1b5eaf421887ff37feca4f7b65
David Whelan <djwhelan@uark.edu>
2026-02-22 06:49:42 -0600

    heartbeat config

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,10 +2,11 @@ import Config
 config :dist, :dns_cluster_query, ["development.local"]
 
 config :libcluster,
+  debug: true,
   topologies: [
-    epmd: [
-      strategy: Cluster.Strategy.Epmd,
-      config: [hosts: [:"dist@192.168.0.189", :"dist@192.168.0.190", :"dist@192.168.0.18"]],
+    gossip: [
+      strategy: Cluster.Strategy.Gossip,
+      config: [],
       connect: {:net_kernel, :connect_node, []},
       disconnect: {:erlang, :disconnect_node, []},
       list_nodes: {:erlang, :nodes, [:connected]}
